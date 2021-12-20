0cbdfb68dab4fe0e26d311e3e0f50111b8d3399f
014ee <014ee@naver.com>
2021-12-20 16:56:16 +0900

    드래그업, 스크롤 영역 기능 구현 완료

--- js/home.js
+++ js/home.js
@@ -5,23 +5,22 @@ var swiper = new Swiper(".history__saving", {
 });
 
 
-// RECENT HISTORY AREA -------------------------------------------------------------------------
+// RECENT HISTORY SCROLL AREA -----------------------------------------------------------------
 const windowY = window.outerHeight
 const tabBarY = document.querySelector('.tab-bar').clientHeight
+const accountTitleY = document.querySelector('.account__title').clientHeight
 const accountHistory = document.querySelector('.account__history')
-const clienRect = accountHistory.getBoundingClientRect(); // 각종 좌표값이 들어있는 객체
-let accountHistoryTop = clienRect.top; // viewport 시작 지점을 기준으로 한 상대좌표 Y값
-const accountHistoryY =  windowY - accountHistoryTop - tabBarY
-const historyRecentTop = document.querySelector('.history__recent').offsetTop
-const historyRecentY = accountHistoryY - historyRecentTop
-
+const accountHistoryTop = accountHistory.offsetTop
+const accountHistoryY =  windowY - tabBarY - accountHistoryTop // 디바이스에 따른 account__history 기본 높이값
 const historyRecent = document.querySelector('.history__recent')
+const historyRecentTop = historyRecent.offsetTop
+const historyRecentY = accountHistoryY - historyRecentTop // 디바이스에 따른 history__recent 기본 높이값
 historyRecent.style.height = `${historyRecentY}px`
+console.log(accountHistoryY) // 가끔 8px이 더해지는 문제 있음 (why?)
 
 
-// TOUCH DRAG --------------------------------------------------------------------------------
+// TOUCH DRAG ---------------------------------------------------------------------------------
 const dragup = document.querySelector('.dragup');
-let currentTranslateY = 0;
 
 let isUp = false; // 드래그업 이벤트 발생 여부 플래그
 let touchInfo = { // touchStart 시점의 좌표와 시간 초기 세팅
@@ -29,6 +28,7 @@ let touchInfo = { // touchStart 시점의 좌표와 시간 초기 세팅
   startTime:0,
   moveY:0,
   translateY: 0,
+  scrollY: historyRecentY,
 }
 
 // 터치 정보값을 초기화하는 함수
@@ -38,6 +38,7 @@ function initTouchInfo(){
   touchInfo.moveY = 0
   touchInfo.translateY = accountHistory.style.transform.slice(11,-3);
   touchInfo.translateY = parseInt(touchInfo.translateY)
+  touchInfo.scrollY = historyRecentY
 }
 
 
@@ -47,28 +48,44 @@ function dragupStart(e){
   touchInfo.startY = e.changedTouches[0].clientY; // 초기 터치된 y값
   touchInfo.startTime = e.timeStamp;
   isUp = true;
+  touchInfo.scrollY = windowY - tabBarY - accountTitleY
+  historyRecent.style.height = `${touchInfo.scrollY}px`
 }
 
 function dragupMove(e){
   if(!isUp) return;
   let nowY = e.changedTouches[0].clientY; // 실시간 터치되고 있는 y값
-  touchInfo.moveY = touchInfo.startY - nowY; // 기준 y에서 이동된 y값
+  touchInfo.moveY = touchInfo.startY - nowY; // 초기 터치 영역으로부터 움직인 y값
   accountHistory.style.transform = `translateY(${ touchInfo.translateY + (-1 * touchInfo.moveY)}px)`
 }
 
 function dragupEnd(e){
   isUp = false
-  if (touchInfo.moveY > 20){
+  if (touchInfo.moveY > 25 || -25 < touchInfo.moveY < 0){
     accountHistory.style.transform = `translateY(-267px)`
-  }else if (touchInfo.moveY < -20){
+    historyRecent.style.height = `${touchInfo.scrollY - historyRecentTop}px`
+    console.log(historyRecentTop)
+  }else if (touchInfo.moveY < -25 || 0 < touchInfo.moveY < 25){
     accountHistory.style.transform = `translateY(0)`
+    initTouchInfo();
+    historyRecent.style.height = `${touchInfo.scrollY}px`
   }
   accountHistory.style.transition = '.4s;'
 }
-
 
 dragup.addEventListener('touchstart', dragupStart)
 dragup.addEventListener('touchmove', dragupMove)
 dragup.addEventListener('touchend', dragupEnd)
 
 
+// MYGOAL CHART PERCENT BG ---------------------------------------------------------------------
+let mygoal = 500000 // 임의의 예시 (목표: 50만원)
+let mygoalCurrent = 290000 // 임의의 예시 (현재: 29만원 저축)
+let mygoalStatus = Math.round((mygoalCurrent/mygoal) * 100) // 현재 목표 달성률
+console.log(mygoalStatus)
+
+let mygoalChart = document.querySelector('.mygoal .chart .bar .status')
+mygoalChart.style.width  = `${mygoalStatus}%`
+
+let savingBoxChart = document.querySelector('.saving-box .status')
+savingBoxChart.style.width  = `${mygoalStatus}%`
